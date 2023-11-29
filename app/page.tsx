bcee5c183d6a81a9d67f445bffda9d3421f8c5e7
mertcetiin <mrtctn506@gmail.com>
2023-11-30 01:19:56 +0300

    header created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 
 
 export default function Home() {
   return (
     <div>
-      Hello
+      <Header />
+
+      <h1>Home</h1>
+
+      <Footer />
     </div>
   )
 }

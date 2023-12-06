d3442d822b2205f118c1f0c4b0e2f44d6802f992
mertcetiin <mrtctn506@gmail.com>
2023-12-06 23:31:09 +0300

    loginPaga basic design added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div>LoginPage</div>
+        <div className="flex items-center justify-center min-h-screen bg-black">
+            <div className="bg-gray-800 p-8 rounded shadow-md w-full max-w-md">
+                <h2 className="text-2xl font-semibold mb-4">Login</h2>
+                <form>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="mt-1 p-2 w-full border rounded-md"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-600">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="mt-1 p-2 w-full border rounded-md"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <button
+                            type="button"
+                            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 

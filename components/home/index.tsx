ff269dd79c31548a735e07d889e2e44087a53e8f
mertcetiin <mrtctn506@gmail.com>
2023-12-07 13:50:26 +0300

    loading added

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,13 +1,30 @@
+"use client"
 import React from 'react'
 import GenresIndex from '../genres';
 import MoviesIndex from '../movies';
+import LoadingIndex from '../loading';
+import { useEffect, useState } from 'react';
+
 
 function HomePage() {
+    const [showLoading, setShowLoading] = useState(true);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setShowLoading(false);
+        }, 2000)
+    }, [])
 
     return (
         <div>
-            <GenresIndex />
-            <MoviesIndex />
+            {showLoading && <LoadingIndex />}
+
+            {!showLoading && (
+                <div>
+                    <GenresIndex />
+                    <MoviesIndex />
+                </div>
+            )}
         </div>
     )
 }
---
+++ components/loading/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoadingIndex() {
+    return (
+        <div className="flex items-center justify-center">
+            <div className="loading border-t-4 border-white border-solid border-opacity-30 rounded-full w-12 h-12 animate-spin"></div>
+        </div>
+    )
+}
+
+export default LoadingIndex;

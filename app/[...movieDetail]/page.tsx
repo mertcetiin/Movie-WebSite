99027f8813bda8794ef5ab31c2ac512fbb067fa3
mertcetiin <mrtctn506@gmail.com>
2023-12-07 13:01:45 +0300

    movieDetail design made

--- app/[...movieDetail]/page.tsx
+++ app/[...movieDetail]/page.tsx
@@ -13,10 +13,10 @@ function MovieDetail() {
             {selectedMovie && (
                 <div key={selectedMovie.id} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     <div className="flex flex-col">
-                        <h2 className="text-3xl font-semibold mb-4">{selectedMovie.title}</h2>
-                        <img className="w-full h-auto mb-4" src={selectedMovie.imgPath} alt={selectedMovie.title} />
-                        <p className="text-gray-600">{selectedMovie.info}</p>
-                        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                        <h2 className="text-xl font-semibold mb-4">{selectedMovie.title}</h2>
+                        <img className="w-full rounded h-auto mb-4" src={selectedMovie.imgPath} alt={selectedMovie.title} />
+                        <p className="text-white text-m">{selectedMovie.info}</p>
+                        <button className="mt-4 w-60 bg-white text-black px-4 py-2 rounded-full hover:bg-gray-800 hover:text-white">
                             Play
                         </button>
                     </div>

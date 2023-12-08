53240b142fea85c540f5aefa0cef07bd3f3d04fe
mertcetiin <mrtctn506@gmail.com>
2023-12-08 12:29:51 +0300

    genresIndex edited

--- components/genres/index.tsx
+++ components/genres/index.tsx
@@ -1,19 +1,20 @@
 "use client";
 import { useMovieStore } from '@/state/store';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 function GenresIndex() {
 
     const genresState = useMovieStore((state) => state.genresState)
 
+
     return (
         <div className="container mx-auto mt-8 flex items-center justify-center">
             <div className='flex flex-wrap gap-4'>
                 {genresState.map((item) => (
-                    <div key={item.id} className="bg-gray-800 p-4 rounded-lg text-white hover:bg-gray-900">
-                        <Link href={item.linkGenres}>{item.name}</Link>
-                    </div>
+                    <Link href={item.linkGenres} key={item.id} className="bg-gray-800 p-4 rounded-lg text-white hover:bg-gray-900">
+                        <p>{item.name}</p>
+                    </Link>
                 ))}
             </div>
         </div>

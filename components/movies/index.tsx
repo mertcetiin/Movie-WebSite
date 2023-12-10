9a89db1c69fd7775773e855026b03f37fe43d3d7
mertcetiin <mrtctn506@gmail.com>
2023-12-10 23:06:46 +0300

    homeIndex api added

--- components/movies/index.tsx
+++ components/movies/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useMovieStore } from '@/state/store';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 
 function MoviesIndex() {
@@ -18,10 +19,30 @@ function MoviesIndex() {
     const popularMovies = moviesState.slice(4, 8)
     const mostMovies = moviesState.slice(8, 12)
 
+    const [imbd, setImdb]: any = useState({})
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/550?api_key=dc694d9a00c5c7954f9d9653af8460f9')
+            .then((res) => res.json())
+            .then((data) => setImdb(data))
+            .catch((error) => console.error('Error fetching data:', error));
+    }, [])
+
 
     return (
         <>
+
             <div className="mt-24 ml-16 mr-16 cursor-pointer">
+                <h2 className="mb-4 text-uppercase text-2xl font-semibold tracking-wider">Favori Movies</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                    <button onClick={() => handleRouter(imbd.imdb_id)} key={imbd.imdb_id} className="overflow-hidden border rounded-lg relative shadow-md transition-transform duration-300 transform hover:scale-125 hover:z-10">
+                        <img className="w-full h-48 object-cover" src={`https://image.tmdb.org/t/p/w500${imbd.backdrop_path}`} alt={imbd.title} />
+                    </button>
+                </div>
+            </div>
+
+
+            {/* <div className="mt-24 ml-16 mr-16 cursor-pointer">
                 <h2 className="mb-4 text-uppercase text-2xl font-semibold tracking-wider">Favori Movies</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                     {favoriteMovies.map((item) => (
@@ -51,7 +72,8 @@ function MoviesIndex() {
                         </button>
                     ))}
                 </div>
-            </div>
+            </div> */}
+
         </>
 
     )

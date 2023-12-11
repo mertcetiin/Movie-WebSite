9ce045fdda69b391f5b288c84bcd1ee747f44b83
mertcetiin <mrtctn506@gmail.com>
2023-12-11 22:22:14 +0300

    apiKey bug fixed

--- components/movies/index.tsx
+++ components/movies/index.tsx
@@ -32,7 +32,7 @@ function MoviesIndex() {
     require('dotenv').config();
     useEffect(() => {
 
-        const apiKey = process.env.REACT_APP_API_KEY;
+        const apiKey = process.env.NEXT_PUBLIC_REACT_APP_API_KEY;
 
         const options = {
             method: 'GET',
@@ -51,11 +51,12 @@ function MoviesIndex() {
 
     return (
         <>
-            <div className="mt-24 ml-16 mr-16 cursor-pointer">
+
+            <div className="mt-20 ml-16 mr-16 cursor-pointer">
                 <h2 className="mb-4 text-uppercase text-2xl font-semibold tracking-wider">Favori Movies</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                     {trendingMovies.map((item) => (
-                        <button onClick={() => handleRouter(item.id)} key={item.id} className="overflow-hidden border rounded-lg relative shadow-md transition-transform duration-300 transform hover:scale-125 hover:z-10">
+                        <button onClick={() => handleRouter(item.id)} key={item.id} className="overflow-hidden border rounded-lg relative shadow-md transition-transform duration-300 transform hover:scale-125">
                             <img className="w-full h-48 object-cover" src={`https://www.themoviedb.org/t/p/w500_and_h282_face${item.backdrop_path}`} alt={item.title} />
                         </button>
                     ))}
@@ -91,4 +92,4 @@ function MoviesIndex() {
     )
 }
 
-export default MoviesIndex
+export default MoviesIndex;

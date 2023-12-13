88a7deaca42c0b35488aa0d00f37c738cbe0fb93
mertcetiin <mrtctn506@gmail.com>
2023-12-13 12:54:32 +0300

    moviesPage created

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+"use client";
+import { useMovieStore } from "@/state/store";
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation';
+
 
 function MoviesPage() {
+
+    const trendingMovies = useMovieStore((state) => state.trendingMovies)
+    const setTrendingMovies = useMovieStore((state) => state.setTrendingMovies)
+
+    const router = useRouter();
+
+    const handleRouter = (id: number) => {
+        useMovieStore.setState({ movieId: id })
+        router.push(`/movieDetail/${id}`)
+    }
+
+
+    useEffect(() => {
+
+        const apiKey = process.env.NEXT_PUBLIC_REACT_APP_API_KEY;
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: apiKey ? `Bearer ${apiKey}` : '',
+            }
+        };
+
+        fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+            .then(res => res.json())
+            .then(data => setTrendingMovies(data.results))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
     return (
-        <div>MoviesPage</div>
+        <div className="mt-20 ml-16 mr-16 cursor-pointer">
+            <h2 className="mb-4 text-uppercase text-2xl font-semibold tracking-wider">Movies</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {trendingMovies.map((item) => (
+                    <button key={item.id} onClick={() => handleRouter(item.id)} className="overflow-hidden border rounded-lg relative shadow-md transition-transform duration-300 transform hover:scale-125 hover:z-10">
+                        <img className="w-full h-48 object-cover" src={`https://www.themoviedb.org/t/p/w500_and_h282_face${item.backdrop_path}`} alt={item.title} />
+                    </button>
+                ))}
+            </div>
+        </div>
     )
 }
 

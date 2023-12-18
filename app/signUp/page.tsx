4302aab9f024385e891999c452de36236e60459f
mertcetiin <mrtctn506@gmail.com>
2023-12-18 19:11:02 +0300

    bug fixed

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -11,7 +11,7 @@ function SignUpPage() {
                 <div className="mt-5">
                     <form action="">
                         <div className="relative mt-6">
-                            <input type="email" name="name" id="name" placeholder="Full Name" className="peer mt-1 w-full border-b-2 text-black border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" />
+                            <input type="text" name="name" id="name" placeholder="Full Name" className="peer mt-1 w-full border-b-2 text-black border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" />
                             <label htmlFor="name" className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Full Name</label>
                         </div>
                         <div className="relative mt-6">

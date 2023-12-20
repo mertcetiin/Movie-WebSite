071d5358ce5c1b08bb6349ce43b2b173569dda36
mertcetiin <mrtctn506@gmail.com>
2023-12-20 19:32:08 +0300

    vercel bug fixed

--- app/[...movieDetail]/not-found.tsx
+++ app/[...movieDetail]/not-found.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function NotFound() {
     return (
         <div className="h-full flex items-center justify-center flex-col">
-            <h1 className="text-2xl font-bold mb-4">We couldn't find the movie you're looking for!</h1>
+            <h1 className="text-2xl font-bold mb-4">We couldn&apos;t find the movie you&apos;re looking for!</h1>
             <Link href="/" className="text-gray-800 underline text-lg">Go home</Link>
         </div>
 

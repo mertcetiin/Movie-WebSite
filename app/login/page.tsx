ce4e8d1596f0f14ae6c5f3715aed68c4c6861359
mertcetiin <mrtctn506@gmail.com>
2023-12-22 13:50:07 +0300

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,7 +25,7 @@ function LoginPage() {
     })
 
     return (
-        <div className="relative mt-40 mb-32 mx-auto w-full max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
+        <div className="relative mt-36 mb-32 mx-auto w-full max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10" >
             <div className="w-full">
                 <div className="text-center">
                     <h1 className="text-3xl font-semibold text-gray-900">Sign in</h1>
